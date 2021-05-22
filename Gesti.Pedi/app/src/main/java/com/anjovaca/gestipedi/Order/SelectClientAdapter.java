693c639ae7cc4b1d2bb3f6ad13de86c76382ea17
Antonio José Valverde Cano <valverde19766@iesmarenostrum.com>
2021-05-22 14:48:30 +0200

    Creación de nuevo añadir pedido y añadir comentarios.

---
+++ Gesti.Pedi/app/src/main/java/com/anjovaca/gestipedi/Order/SelectClientAdapter.java
@@ -0,0 +1,4 @@
+package com.anjovaca.gestipedi.Order;
+
+public class SelectClientAdapter {
+}
---
+++ Gesti.Pedi/app/src/main/java/com/anjovaca/gestipedi/Order/SelectClientForOrder.java
@@ -0,0 +1,14 @@
+package com.anjovaca.gestipedi.Order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectClientForOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_client_for_order);
+    }
+}
